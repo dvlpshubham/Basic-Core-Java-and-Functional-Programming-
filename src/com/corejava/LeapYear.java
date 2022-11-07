1a4977227457c93490e1c3cb9ccb342c6e7aae1d
Shubham Raj <shubhamaryan805@gmail.com>
2022-11-07 22:19:57 +0530

    Added: Logic to only check leap year if year is of 4 digits

---
+++ src/com/corejava/LeapYear.java
@@ -0,0 +1,30 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+
+    public static void checkLeapYear(int year) {
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0 && year > 1581))
+            System.out.println(year + " is a leap year");
+        else
+            System.out.println(year + " is not a leap year");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Leap Year Checker Program!!" + "\n" + "Enter a year you want to check:");
+        int year;
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+
+        if (year > 999 && year < 10000) {
+            checkLeapYear(year);
+        } else {
+            System.out.println("Enter a 4 digit year");
+        }
+
+    }
+}
